629faaaf1d1627bed7ea0308fde2d2687c17507e
TienHao <tienhaochang703@gmail.com>
2017-12-25 18:33:04 +0800

    12/25 未完成

---
+++ LeetCode/src/com/leetcode/easy/Easy67AddBinery.java
@@ -0,0 +1,15 @@
+package com.leetcode.easy;
+
+public class Easy67AddBinery {
+
+    public String addBinary(String a, String b) {
+    	a.toCharArray();
+        StringBuffer result = new StringBuffer();
+        //result.
+    	return null;
+    }
+	public static void main(String[] args) {
+
+	}
+
+}
